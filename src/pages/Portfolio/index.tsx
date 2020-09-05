f4281d941212af19605ff5c25960045ca065c344
marco-amorim <marcoamorim95@hotmail.com>
2020-09-05 19:40:28 -0300

    portfolio page

--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -1,13 +1,71 @@
 import React, { useEffect } from 'react';
 
 import './styles.css';
+import Title from '../../components/Title';
 
 const Portfolio = () => {
 	useEffect(() => {
 		document.getElementById('page-footer')?.classList.add('footer-fixed');
 	});
 
-	return <div>Portfolio!</div>;
+	return (
+		<section id="portfolio" className="container">
+			<Title text="Personal projects" />
+			<li>
+				<div className="project-link">
+					<a
+						href="https://github.com/marco-amorim/travel-log-app"
+						rel="noopener noreferrer"
+						target="_blank"
+					>
+						Travel Log App
+					</a>
+				</div>
+				A full stack application to store and list places you have traveled
+			</li>
+
+			<li>
+				<div className="project-link">
+					<a
+						href="https://github.com/marco-amorim/NLW-02"
+						rel="noopener noreferrer"
+						target="_blank"
+					>
+						Proffy
+					</a>
+				</div>
+				A learning platform for students and teachers to schedule private
+				classes
+			</li>
+
+			<li>
+				<div className="project-link">
+					<a
+						href="https://github.com/marco-amorim/personal-agenda"
+						rel="noopener noreferrer"
+						target="_blank"
+					>
+						Personal Agenda
+					</a>
+				</div>
+				Desktop application for managing contacts and meetings
+			</li>
+
+			<li>
+				<div className="project-link">
+					<a
+						href="https://github.com/marco-amorim/website"
+						rel="noopener noreferrer"
+						target="_blank"
+					>
+						Website
+					</a>
+				</div>
+				Personal Website for portfolio and networking, the whole code for this
+				webiste
+			</li>
+		</section>
+	);
 };
 
 export default Portfolio;
